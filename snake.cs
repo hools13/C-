068f50fa5f3461341b7faf25c194b3453fe8a484
Mykhailo <in.studio.drp@gmail.com>
2016-08-31 23:11:20 +0200

    Class Figure

--- snake.cs
+++ snake.cs
@@ -1,25 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
 
-namespace Snake
-{
-	class Program
+	namespace Snake
+	{
+		class Program
 	{
 		static void Main( string[] args )
         {
+        	Console.SetBufferSize( 80, 25 );
+
+
+
+			/* 
 			Point p1 = new Point(1, 3, '*');
 			p1.Draw();
-
 			Point p2 = new Point(4, 5, '#');
-			p2.Draw();
+			p2.Draw();  
+			*/
 
-			HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-			line.Draw();
+			//отрисовка рамки
 
-			Console.ReadLine();
+			HorizontalLine upLine = new HorizontalLine( 0, 78, 0, '+');
+			HorizontalLine downLine = new HorizontalLine( 0, 78, 24, '+');
+			VerticalLine leftLine = new VerticalLine( 0, 24, 0, '+');
+			VerticalLine rightLine = new VerticalLine( 0, 24, 78, '+');
+			upLine.Draw();
+			downLine.Draw();
+			leftLine.Draw();
+			rightLine.Draw();
+
+
+			// отрисовка точек
+			Point p = new Point(4, 5, '*');
+			p.Draw();
 		}
 	}
 }
