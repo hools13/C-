9b3eae970d61ac3accf18d76d9551197b04c7ae7
Mykhailo <in.studio.drp@gmail.com>
2016-09-03 21:00:45 +0200

    Move snake forward

--- moveSnake.cs
+++ moveSnake.cs
@@ -8,14 +8,36 @@ namespace Snake
 {
 	class Snake : Figure
 	{
+		Direction direction;
+
 		public Snake(Point tail, int lenght, Direction direction )
 		{
+			direction = _direction;
 			for(int i = 0; i < lenght; i++)
 			{
 				Point p = new Point( tail );
 				p.Move( i, direction );
 				pList.Add( p );
 			}
+		}
+
+		internal void Move()
+		{
+			Point tail = pList.First();
+			pList.Remove( tail );
+			Point head = GetNextPoint();
+			pList.Add( head );
+
+			tail.Clear();
+			head.Draw();
+		}
+
+		public Point GerNextPoin()
+		{
+			Point head = pList.Last();
+			Point nextPoint = new Point( head );
+			nextPoint.Move( 1, direction );
+			return nextPoint;
 		}
 	}
 }
--- snake.cs
+++ snake.cs
@@ -37,6 +37,7 @@
 			Point p = new Point(4, 5, '*');
 			Snake snake = new Snake(p, 4, Direction.RIGHT);
 			snake.Draw();
+			snake.Move();
 		}
 	}
 }
